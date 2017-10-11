53715aa22cd3e1f7aa965ac85ef0213c6e1265c4
Maggie <margaret.schweihs@gmail.com>
2017-10-11 14:59:19 -0700

    mapper3 and reducer3 done
    similar to a word count, but takes a string and rearranges the letters
    to be in alphabetical order, co CFMD and FMDC both come out as CDFM.
    Then the reducer summarizes.

---
+++ mapper3.py
@@ -0,0 +1,18 @@
+#File: mapper3.py
+#Author: Maggie Schweihs
+#DS730, Fall 2017
+#Project2: Hadoop
+
+import sys
+import re
+
+def main(argv):
+    line = sys.stdin.readline()
+    pattern = re.compile("[a-zA-Z0-9]+")
+    while line:
+    #for line in sys.stdin:
+        for word in pattern.findall(line):
+            print(''.join(sorted(word))+"\t"+"1")
+        line = sys.stdin.readline()
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ reducer3.py
@@ -0,0 +1,27 @@
+#File: reducer3.py
+#Author: Maggie Schweihs
+#DS730, Fall 2017
+#Project2: Hadoop
+#!/usr/bin/env python3
+
+import sys
+
+def main(argv):
+    current_word = None
+    current_count = 0
+    word = None
+    for line in sys.stdin:
+        line = line.strip()
+        word, count = line.split('\t', 1)
+        count = int(count)
+        if current_word == word:
+            current_count += count
+        else:
+            if current_word:
+                print(current_word + ':' + str(current_count))
+            current_count = count
+            current_word = word
+    if current_word == word:
+        print(current_word + ':' + str(current_count))
+if __name__ == "__main__":
+    main(sys.argv)
