0e0c8a34ed8fbb3bcacdcec115b6b495190d997c
Maggie <margaret.schweihs@gmail.com>
2017-10-11 13:01:41 -0700

    mapper 2 done
    mapper2 evaluates words for palindromedness. It determines how many
    edits are needed to make a word a palindrome and prints (key, value) as
    (#swaps, word)

---
+++ mapper2.py
@@ -0,0 +1,43 @@
+
+#File: mapper2.py
+#Author: Maggie Schweihs
+#DS730, Fall 2017
+#Project2: Hadoop
+
+import sys
+
+def main(argv):
+#read line from std in
+    line = sys.stdin.readline()
+    while line:
+        line = line.split()
+        for word in line:
+            swaps = 0
+        #Check if word has odd number of letters
+            if len(word) % 2 != 0: 
+                j = 1
+                i = int(len(word)/2) 
+        #Count how many swaps to make a palindrome by counting the letters that are not symmetrical about the middle letter.
+                while j <= i:
+                    if word[int(i + j)] != word[int(i-j)]:
+                        swaps += 1
+                        j += 1
+                    else:
+                        j += 1
+        #Otherwise, word has even number of letters
+            else:
+                j = 0
+                i = int(len(word)/2)
+        #Count the number of swaps to make palindrome
+                while j < i:
+                    if word[int(i + j)] != word[int(i-j-1)]:
+                        swaps +=1
+                        j += 1
+                    else:
+                        j += 1
+        #Output (key, value) as (word, swaps)
+            print(word + '\t' + str(swaps))
+        line = sys.stdin.readline()
+if __name__ == "__main__":
+    main(sys.argv)
+
